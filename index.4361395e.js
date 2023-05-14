1ee4ca562b55d38074afeb8b91ab279cc475625b
m1lky4 <125802820+m1lky4@users.noreply.github.com>
2023-05-14 15:57:17 +0000

    Deploying to gh-pages from @ m1lky4/cargo-project@bfe740be7900010f4b042a41253e77d6196181fd 🚀

--- index.4361395e.js
+++
@@ -1,2 +0,0 @@
-var e=document.getElementById("openModalBtn"),t=document.getElementById("modalMenu");e.addEventListener("click",(function(){t.style.display="block"})),document.addEventListener("click",(function(n){t.contains(n.target)||n.target===e||(t.style.display="none")}));document.querySelector(".video").oncontextmenu=function(e){e.preventDefault()};
-//# sourceMappingURL=index.4361395e.js.map
--- index.8163fd24.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("openModalBtn"),n=document.getElementById("modalMenu");e.addEventListener("click",(function(){n.style.display="block"})),document.addEventListener("click",(function(t){n.contains(t.target)||t.target===e||(n.style.display="none")})),document.querySelector(".video").oncontextmenu=function(e){e.preventDefault()}}();
-//# sourceMappingURL=index.8163fd24.js.map
