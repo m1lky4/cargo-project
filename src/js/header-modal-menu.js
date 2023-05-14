b15857571d617e08ac1f1c98713ba65378c804b3
kolba00 <vr.nvkv00@gmail.com>
2023-05-14 14:43:11 +0300

    added modal menu

---
+++ src/js/header-modal-menu.js
@@ -0,0 +1,12 @@
+var openModalBtn = document.getElementById('openModalBtn');
+var modalMenu = document.getElementById('modalMenu');
+
+openModalBtn.addEventListener('click', function() {
+  modalMenu.style.display = 'block';
+});
+
+document.addEventListener('click', function(event) {
+  if (!modalMenu.contains(event.target) && event.target !== openModalBtn) {
+    modalMenu.style.display = 'none';
+  }
+});
