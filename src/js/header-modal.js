9256acff8ecc41ef2f18455fc1b56c27aa3581ca
m1lky4 <temanafanin1337@gmail.com>
2023-05-19 21:20:29 +0200

    doing new modal

--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -1,10 +1,11 @@
 const openModalBtn = document.querySelector('.open-modal-btn')
-const modal = document.querySelector('.popup');
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal-content');
 const body = document.querySelector('body');
-
 openModalBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    modal.classList.add('target');
+    backdrop.classList.remove('is-hidden');
+    modal.classList.add('is-open');
     body.classList.add('modal-is-open');
     scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
     window.scrollTo(0, scrollPosition);
