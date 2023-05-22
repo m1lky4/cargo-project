17fa5e81419b994ca12c63083263aa17f92b0752
m1lky4 <temanafanin1337@gmail.com>
2023-05-22 19:47:52 +0200

    created modal

--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -1,15 +1,41 @@
-const openModalBtn = document.querySelector('.open-modal-btn')
+const openModalBtn = document.querySelector('.open-modal-btn');
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal-content');
 const body = document.querySelector('body');
-openModalBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    backdrop.classList.remove('is-hidden');
-    modal.classList.add('is-open');
-    body.classList.add('modal-is-open');
-    scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    window.scrollTo(0, scrollPosition);
-})
+const close = document.querySelector('.close-icon')
+openModalBtn.addEventListener('click', e => {
+  e.preventDefault();
+  scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  window.scrollTo(0, scrollPosition);
+  backdrop.classList.remove('is-hidden');
+  modal.classList.add('is-open');
+  body.classList.add('modal-is-open');
+  backdrop.addEventListener('click', closeModal);
+  document.addEventListener('keydown', closeModalOnEscape);
+});
 
+function closeModalOnEscape(e) {
+  if (e.code === 'Escape') {
+    backdrop.classList.add('is-hidden');
+    modal.classList.remove('is-open');
+    body.classList.remove('modal-is-open');
+    backdrop.removeEventListener('click', closeModal);
+    document.removeEventListener('keydown', closeModalOnEscape);
+  } 
+}
+function closeModal(e) {
+  console.log(e.target);
+  if (e.target === backdrop) {
+    backdrop.classList.add('is-hidden');
+    modal.classList.remove('is-open');
+    body.classList.remove('modal-is-open');
+    backdrop.removeEventListener('click', closeModal);
+  } else if (e.target === close) {
+    backdrop.classList.add('is-hidden');
+    modal.classList.remove('is-open');
+    body.classList.remove('modal-is-open');
+    backdrop.removeEventListener('click', closeModal);
+  }
+}
 
 // closeModalBtn.addEventListener()
